27a165e5ab18238ff38874c0a23c4c34c2a147a8
Shen Yiming <soimy@163.com>
2018-01-03 23:51:30 +0800

    update npm add yargs cli frontend

--- cli.js
+++ cli.js
@@ -5,34 +5,63 @@ const generateBMFont = require('./index');
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
-const args = require('commander');
+// const args = require('commander');
 const utils = require('./lib/utils');
 
-args
-  .version('msdf-bmfont-xml v' + pjson.version)
-  .usage('[options] <font-file>')
-  .arguments('<font_file>')
-  .description('Creates a BMFont compatible bitmap font of signed distance fields from a font file')
-  .option('-f, --output-type <format>', 'font file format: xml(default) | json', 'xml')
-  .option('-o, --filename <atlas_path>', 'filename of font textures (defaut: font-face) font filename always set to font-face name')
-  .option('-s, --font-size <fontSize>', 'font size for generated textures (default: 42)')
-  .option('-i, --charset-file <charset>', 'user-specified charactors from text-file')
-  .option('-m, --texture-size <w,h>', 'Width/Height of generated textures (default: 512,512)', (v) => {return v.split(',')})
-  .option('-p, --texture-padding <n>', 'padding between glyphs (default: 1)')
-  .option('-r, --distance-range <n>', 'distance range for SDF (default: 4)')
-  .option('-t, --field-type <type>', 'msdf(default) | sdf | psdf | svg')
-  .option('-d, --round-decimal <digit>', 'rounded digits of the output font file. (Defaut: 0)', 0)
-  .option('-v, --vector', 'generate svg vector file for debuging')
-  .option('-u, --reuse <file.cfg>', 'use old config to append font, ommit file to save new cfg')
-  .option('    --tolerance <value>', 'use point tolerance to filter problematic vector shape (Defaut: 0)', 0)
-  .option('    --smart-size', 'shrink atlas to the smallest possible square (Default: false)', false)
-  .option('    --pot', 'atlas size shall be power of 2 (Default: false)', false)
-  .option('    --square', 'atlas size shall be square (Default: false)', false)
-  .action(function(file){
-    fontFile = file;
-  }).parse(process.argv);
+const yargs = require('yargs');
 
-const opt = args.opts();
+yargs
+  .usage('$0 [options] <font-file>', 'Creates a BMFont compatible bitmap font of signed distance fields from a font file', (yargs) => {
+    yargs.positional('font-file', { type: 'string', desc: 'Font file, supported format: OTF, TTF, WOFF' })
+  })
+  .example('$0 -o path/to/atlas.png -m 512,256 -s 42 -t msdf path/to/font.ttf', 'generate a 512x256 multichannel-sdf atlas with font-size=42')
+  .locale('en')
+  .options({
+    'output-type':    { alias: 'f', type: 'string', choice: ['xml', 'json'], requiresArg: true, default: 'xml', desc: 'font-file format' },
+    'filename':       { alias: 'o', type: 'string', requiresArg: true, desc: 'filename of font textures (defaut: font-face) font filename always set to font-face name'},
+    'font-size':      { alias: 's', type: 'number', requiresArg: true, default: 42, desc: 'font size for generated textures (default: 42)' },
+    'charset-file':   { alias: 'i', type: 'string', desc: 'user-specified charactors from text-file' },
+    'texture-size':   { alias: 'm', type: 'array', requiresArg: true, default: [512, 512], desc: 'user-specified charactors from text-file' },
+    'texture-padding':{ alias: 'p', type: 'number', requiresArg: true, default: 1, desc: 'padding between glyphs (default: 1)' },
+    'distance-range': { alias: 'r', type: 'number', requiresArg: true, default: 1, desc: 'distance range for SDF (default: 4)' },
+    'field-type':     { alias: 't', type: 'string', choice: ['msdf', 'sdf', 'psdf', 'svg'], requiresArg: true, default: 'msdf', desc: 'msdf(default) | sdf | psdf | svg' },
+    'round-decimal':  { alias: 'd', type: 'number', requiresArg: true, default: 0, desc: 'rounded digits of the output font file. (Defaut: 0)' },
+    'vector':         { alias: 'v', type: 'boolean', desc: 'generate svg vector file for debuging' },
+    'reuse':          { alias: 'u', type: 'string', desc: 'save/create config file for reusing settings' },
+    'smart-size':     { type: 'boolean', group: 'Atlas control', desc: 'shrink atlas to the smallest possible square (Default: false)' },
+    'pot':            { type: 'boolean', group: 'Atlas control', desc: 'atlas size shall be power of 2 (Default: false)' },
+    'square':         { type: 'boolean', group: 'Atlas control', desc: 'atlas size shall be square (Default: false)' },
+  })
+  .version()
+  .help()
+  .argv;
+
+// args
+//   .version('msdf-bmfont-xml v' + pjson.version)
+//   .usage('[options] <font-file>')
+//   .arguments('<font_file>')
+//   .description('Creates a BMFont compatible bitmap font of signed distance fields from a font file')
+//   .option('-f, --output-type <format>', 'font file format: xml(default) | json', 'xml')
+//   .option('-o, --filename <atlas_path>', 'filename of font textures (defaut: font-face) font filename always set to font-face name')
+//   .option('-s, --font-size <fontSize>', 'font size for generated textures (default: 42)')
+//   .option('-i, --charset-file <charset>', 'user-specified charactors from text-file')
+//   .option('-m, --texture-size <w,h>', 'user-specified charactors from text-file', (v) => {return v.split(',')})
+//   .option('-p, --texture-padding <n>', 'padding between glyphs (default: 1)')
+//   .option('-r, --distance-range <n>', 'distance range for SDF (default: 4)')
+//   .option('-t, --field-type <type>', 'msdf(default) | sdf | psdf | svg')
+//   .option('-d, --round-decimal <digit>', 'rounded digits of the output font file. (Defaut: 0)', 0)
+//   .option('-v, --vector', 'generate svg vector file for debuging')
+//   .option('-u, --reuse <file.cfg>', 'save/create config file for reusing settings')
+//   .option('    --tolerance <value>', 'use point tolerance to filter problematic vector shape (Defaut: 0)', 0)
+//   .option('    --smart-size', 'shrink atlas to the smallest possible square (Default: false)', false)
+//   .option('    --pot', 'atlas size shall be power of 2 (Default: false)', false)
+//   .option('    --square', 'atlas size shall be square (Default: false)', false)
+//   .action(function(file){
+//     fontFile = file;
+//   }).parse(process.argv);
+
+const opt = yargs.argv;
+opt.fontFile = opt._[0];
 utils.roundAllValue(opt); // Parse all number from string
 const keys = Object.keys(opt)
 const padding = longestLength(keys) + 2;
@@ -43,7 +72,7 @@ keys.forEach(key => {
 });
 console.log("========================================");
 
-if (typeof fontFile === 'undefined') {
+if (typeof opt.fontFile === 'undefined') {
   console.error('No font file specified, aborting.... use -h for help');
   process.exit(1);
 }
@@ -53,7 +82,7 @@ fs.readFile(opt.charsetFile || '', 'utf8', (error, data) => {
   }
   if (data) opt.charset = data;
   
-  generateBMFont(fontFile, opt, (error, textures, font) => {
+  generateBMFont(opt.fontFile, opt, (error, textures, font) => {
     if (error) throw error;
     textures.forEach((texture, index) => {
       if (opt.vector) {
